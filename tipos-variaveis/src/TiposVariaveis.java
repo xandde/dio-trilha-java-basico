3cdecc2721604bdd54494a573cefd6c111ceb3e4
xandde <alexandrepitta00@gmail.com>
2024-07-14 21:57:03 -0300

    aulas de tipos de dados

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500; //Assim que se atribui valor MILHAR á variável.
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; //Se começar com zero, talvez seja necessário usar 'string' como tipo da variável
+        long cpf = 9876543210L; // se começar com zero, talvez seja necessário usar 'string' como tipo da variável
+        float pi = 3.14F;
+        double salario = 1275.33;
     }
 }
