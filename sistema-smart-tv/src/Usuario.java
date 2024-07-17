0e615c8e920696cc8afd9e477e55e187fd01dee6
xandde <alexandrepitta00@gmail.com>
2024-07-17 14:09:48 -0300

    Iniciando estudos de função

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual ? :" + smartTv.canal);
+
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+        System.out.println("Canal atual ? :" + smartTv.canal);
+        System.out.println("Volume atual ? :" + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+    }
+}
